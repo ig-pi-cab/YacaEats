70b68e828340e7f8bf73fecf4e60147b3d7f320f
ig-pi-cab <ig.pi@duocuc.cl>
2022-10-31 11:29:37 -0300

    slider fotos y controles

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-let carouselWidth = $('.carousel-inner')[0].scrollWidth;
-let cardWidth = $('.carousel-item').width();
-
-let scrollPosition = 0;
-
-$('.carousel-control-next').on('click', function(){
-    if(scrollPosition < (carouselWidth - (cardWidth * 4))){
-        console.log('next')
-        scrollPosition = scrollPosition + cardWidth;
-        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600)
-    }
-});
-
-$('.carousel-control-previous').on('click', function(){
-    if(scrollPosition > 0){
-        console.log('prev')
-        scrollPosition = scrollPosition - cardWidth;
-        $('.carousel-inner').animate({scrollLeft: scrollPosition}, 600)
-    }
+$('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<span class="prev_arrow"><i class="fa-solid fa-arrow-left"></i></span>',
+    nextArrow: '<span class="next_arrow"><i class="fa-solid fa-arrow-right"></i></span>',
+    adaptiveHeight: true
 });
